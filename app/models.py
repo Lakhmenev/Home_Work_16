7a9ee368e48ecff927b21cbe455a916420d6f066
dmitrii_lakhmenev <lahmenev@gmail.com>
2022-04-02 16:07:57 +0300

    Edit class UserRole

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class UserRole(db.Model):
     __tablename__ = 'user_roles'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), primary_key=True)
+    name = db.Column(db.String(100), unique=True)
 
 
 class User(db.Model):
